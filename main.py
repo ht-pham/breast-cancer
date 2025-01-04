e8b784bd0a5358736d7fe88226ec1eb52fad9a76
Huong Pham <96hpham@gmail.com>
2025-01-03 21:16:38 -0600

    Added Next Steps section

--- main.py
+++ main.py
@@ -54,21 +54,6 @@ def predict():
     res=[castVote(predictions=predicts)]
     res.extend(list(map(isBenign,predicts)))
 
-    """ results = {
-        'knn_prediction': knn_prediction,
-        'knn_confidence': 85.2,
-        'knn_accuracy': 90.5,
-        'svm_prediction': svm_prediction,
-        'svm_confidence': 88.7,
-        'svm_accuracy': 92.0,
-        'decision_tree_prediction': dt_prediction,
-        'decision_tree_feature_importance': 'Radius Mean: 0.45, Texture Mean: 0.30',
-        'decision_tree_accuracy': 89.8,
-        'neural_network_prediction': nn_prediction,
-        'neural_network_confidence': 92.4,
-        'neural_network_accuracy': 94.5
-    } """
-
     html_names = ['Overall_prediction','knn_prediction','svm_prediction','decision_tree_prediction','neural_network_prediction']
     results = dict(zip(html_names,res))
     return render_template('result.html', **results)
@@ -78,12 +63,12 @@ def isBenign(predicted):
         return "Benign"
     else:
         return "Malignant"
+    
 def castVote(predictions):
     # use majority vote rather average threshold to conclude because y_pred returns 0 or 1 
     benign = predictions.count(0)
     malignant = predictions.count(1)
     return "Benign" if benign > malignant else "Malignant"
-
 
 # Run the app
 if __name__ == '__main__':
